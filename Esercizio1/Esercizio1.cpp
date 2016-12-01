69c3f09760e6bfbeef3678834044301978ef9ec0
MaddoScientisto <maddo.science@gmail.com>
2016-12-01 11:38:15 +0100

    Finito esercizio 1

--- Esercizio1/Esercizio1.cpp
+++ Esercizio1/Esercizio1.cpp
@@ -3,6 +3,8 @@
 #include "MaddoLib.h"
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <time.h>
 using namespace std;
 using namespace Maddo;
 //Dichiarazione funzioni
@@ -15,7 +17,7 @@ void PrintVector(vector<string> inventory, vector<string>::const_iterator iter);
 
 int main()
 {
-	//srand(static_cast<unsigned int>(time(0))); // Inizializzare il generatore di numeri casuali
+	srand(static_cast<unsigned int>(time(0))); // Inizializzare il generatore di numeri casuali
 	bool exit = false;
 	while (!exit) {
 		Game();
@@ -31,12 +33,68 @@ void Game()
 
 	vector<string>::iterator iterator; 
 	vector<string>::const_iterator readOnlyIterator;
+	string str;
 
 	InitializeCargoHold(inventory);
 	PrintVector(inventory, readOnlyIterator);
 
-	MaddoLib::pause
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
 
+	 str = inventory.back(); // back e non end
+	inventory.pop_back();
+
+	MaddoLib::OutputLine(str + " non era legato bene ed è caduto in acqua");
+	PrintVector(inventory, readOnlyIterator);
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	inventory.push_back("tè");
+	MaddoLib::OutputLine("Un mercante ti dona del tè");
+	PrintVector(inventory, readOnlyIterator);
+
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	MaddoLib::OutputLine("Arrivi ad un mercato e scambi lana con frumento");
+
+	iterator = find(inventory.begin(), inventory.end(), "lana");
+	*iterator = "frumento";
+
+	PrintVector(inventory, readOnlyIterator);
+
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	MaddoLib::OutputLine("Dei pirati ti attaccano e ti rubano il vino");
+	iterator = find(inventory.begin(), inventory.end(), "vino");
+	inventory.erase(iterator);
+
+	PrintVector(inventory, readOnlyIterator);
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	MaddoLib::OutputLine("Ordini l'inventario in ordine alfabetico");
+	sort(inventory.begin(), inventory.end());
+	PrintVector(inventory, readOnlyIterator);
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	random_shuffle(inventory.begin(), inventory.end());
+	MaddoLib::OutputLine("Una tempesta mescola tutto il carico");
+	PrintVector(inventory, readOnlyIterator);
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
+
+	MaddoLib::OutputLine("Infine giungi al mercato e vendi tutto il tuo carico");
+
+	for (auto & element : inventory) {
+		element = "oro";
+	}
+
+	PrintVector(inventory, readOnlyIterator);
+	MaddoLib::Pause();
+	MaddoLib::ClearScreen();
 }
 
 void InitializeCargoHold(vector<string>& inventory)
@@ -51,8 +109,9 @@ void InitializeCargoHold(vector<string>& inventory)
 
 void PrintVector(vector<string> inventory, vector<string>::const_iterator iter) 
 {
-	MaddoLib::OutputLine("your items: ");
+	MaddoLib::OutputLine("Carico corrente della nave: ");
 	for (iter = inventory.begin(); iter != inventory.end(); ++iter) {
-		MaddoLib::OutputLine(*iter); // puntatore di deferenziazione, permette di accedere al contenuto della cella
+		MaddoLib::OutputLine(*iter);
 	}
 }
+
